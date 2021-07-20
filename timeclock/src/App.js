991316bf5d7a04c61ce1c0f78c3b44a631e01ec4
Cody Michaud <cody.r.michaud@gmail.com>
2021-07-20 12:24:06 -0700

    Finished app

--- timeclock/src/App.js
+++ timeclock/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 class TimeofDay extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { date: new Date() };
+  }
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+    const oneSec = 1000;
+    setInterval(() => {
+      this.setState({ date: new Date() });
+    }, oneSec);
+  }
 }
 
 
 
-export default App;
+export default TimeofDay;
